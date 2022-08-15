ebf3e0e5d0168815592db494b231d4fb1c6d0d5a
Lucas Gomes Roseira <41271185+LucasRoseira@users.noreply.github.com>
2022-08-14 23:26:19 -0300

    First

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset = "UTF-8">
+	<link rel="stylesheet" type="text/css" href="assets/index.css">
+
+	<title> Celke - Classes and Objects</title>
+</head>
+
+<body> 
+	<header></header>
+	<main>
+		<div>
+			<?php
+
+			require 'user2.php';
+			$user = new User();
+			//We're passing the parameters to the method register and printing the result to the screen
+			echo $user->register("Lucas","lucas.roseira@fatec.sp.gov.br","IT support", 26);
+		
+
+			//I'm killing the page here just to reuse the structure because the below code is the first one.
+			die();
+			?>
+
+			<?php 
+			/*we've to call the other file that contais our class*/
+			require 'user.php';
+
+			/*'new' is a reserved word so you can't used it.*/
+			$user = new User();
+
+			/*When we call the method it'll simply return the phrase*/
+			$msg = $user->register();
+			echo $msg;
+			?>
+		</div>
+	</main>
+
+</body>
+</html>
