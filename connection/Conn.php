562c733b700bbd6dba85058b233602f9a9269b5a
Lucas Gomes Roseira <41271185+LucasRoseira@users.noreply.github.com>
2022-08-21 00:13:05 -0300

    fix: grammar

--- connection/Conn.php
+++ connection/Conn.php
@@ -8,14 +8,14 @@ class Conn {
 	public $pass = "";
 	public $dbname = "lucas"; //public $dbaname = "lucas"; Notice: Undefined property: Conn::$dbname in C:\xampp\htdocs\poo php\Conn.phpon line 18
 	public $port = 3306; //the port isn't required everytime
-	public $connect = null; //by default, it starts empty
+	public $connect = null; //by default, starts empty
 
 	public function connect () {
 		try {
-			/*I'm using PDO(PHP Data Objects) to improve my skills, but it's better to use Laravel, Magento, Drupal, Zend, Wordpress for real projects or you can other language and their frameworks!
+			/*/*I'm using PDO(PHP Data Objects) to improve my skills, but it's better to use Laravel, Magento, Drupal, Zend, or WordPress for real projects or you can use any other languages and it's frameworks.
 			*/
 			$this->connect = new PDO("mysql:host=" . $this->host .";dbname="
-			 . $this->dbname, $this->user, $this->pass); //The port was removed and it's working.
+			 . $this->dbname, $this->user, $this->pass); //the port was removed and it's working
 	
 			echo "Successful connection with database!<br>";
 			return $this->connect;
